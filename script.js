9e3441503553f7b0733a5a7ee2b1540b3e8c5601
antonkornilov-ua <83725472+antonkornilov-ua@users.noreply.github.com>
2022-04-08 12:40:54 +0300

    trouble

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-console.log('числа');
-console.log(`MAX_SAFE_INTEGER ${Number.MAX_SAFE_INTEGER}`);
 
+// // Test задача 1
+// let text = prompt("Enter some text");
+// let reverseText = text.split('').reverse().join('');
+// alert(reverseText);
+let textInput = document.querySelector("input");
+let button = document.querySelector("buttonreverse").addEventListener('click', reverseText)
+let outpuText = document.querySelector("output");
 
-const stringInt = '42';
-console.log(parseInt(stringInt)+2); //44
-
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min +1 ) + min)
-    
+reverseText () {
+let reText = textInput.split('').reverse().join('');
+return outpuText.innerHTML(reText)
 }
-console.log('Random number is :' + getRandomNumber(1, 100));
-
-
-
-// Test задача 1
-let text = prompt("Enter some text");
-let reverseText = text.split('').reverse().join('');
-alert(reverseText);
